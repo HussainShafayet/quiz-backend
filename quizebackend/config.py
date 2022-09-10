33281cea98edf9f2ae2e3c6035919587e3c8d8ee
HussainShafayet <42945427+HussainShafayet@users.noreply.github.com>
2022-09-10 23:23:04 +0600

    database connect

---
+++ quizebackend/config.py
@@ -0,0 +1,7 @@
+import os
+
+os.environ['DB_NAME'] = 'quiz'
+os.environ['DB_USER'] = 'postgres'
+os.environ['DB_PASSWORD'] = 'psql123'
+os.environ['DB_HOST'] = 'localhost'
+os.environ['DB_PORT'] = '5432'
--- quizebackend/quizebackend/settings.py
+++ quizebackend/quizebackend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
 from pathlib import Path
+import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -99,8 +100,12 @@ WSGI_APPLICATION = 'quizebackend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT')
     }
 }
 

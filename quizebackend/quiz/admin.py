fcba2873a3404e3e208a53223c860dd70f2af0ec
HussainShafayet <42945427+HussainShafayet@users.noreply.github.com>
2022-09-11 00:46:46 +0600

    quiz model create

--- quizebackend/quiz/admin.py
+++ quizebackend/quiz/admin.py
@@ -1,3 +1,8 @@
+from tkinter.messagebox import QUESTION
 from django.contrib import admin
-
+from .models import Category,Question,Option
 # Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Question)
+admin.site.register(Option)
--- quizebackend/quiz/models.py
+++ quizebackend/quiz/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Category(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.title
+    
+class Question(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.title
+    
+class Option(models.Model):
+    question = models.ForeignKey(Question,on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.title

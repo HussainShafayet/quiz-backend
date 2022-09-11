3fdc858759206371068625b1059bf07ff6eac17c
HussainShafayet <42945427+HussainShafayet@users.noreply.github.com>
2022-09-12 01:16:33 +0600

    answer model added

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,8 +1,9 @@
 from tkinter.messagebox import QUESTION
 from django.contrib import admin
-from .models import Category,Question,Option
+from .models import Answer, Category,Question,Option
 # Register your models here.
 
 admin.site.register(Category)
 admin.site.register(Question)
 admin.site.register(Option)
+admin.site.register(Answer)
--- quiz/models.py
+++ quiz/models.py
@@ -1,3 +1,5 @@
+from pyexpat import model
+from unicodedata import category
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -10,6 +12,7 @@ class Category(models.Model):
         return self.title
     
 class Question(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     
@@ -17,8 +20,20 @@ class Question(models.Model):
         return self.title
     
 class Option(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     question = models.ForeignKey(Question,on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
+    isCheckd = models.BooleanField(False)
+    
     
     def __str__(self):
         return self.title
+    
+class Answer(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question,on_delete=models.CASCADE)
+    option = models.OneToOneField(Option, on_delete=models.CASCADE)
+    isCorrect = models.BooleanField(False)
+    
+    
